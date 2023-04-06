f5a0e5d1922421b872f44cb1e7ae95f4ede380ee
Lahiru-Randika <130041725+Lahiru-Randika@users.noreply.github.com>
2023-04-06 16:52:37 +0530

    Implement Stack and its functions using Array

--- ArrayStack.java
+++ ArrayStack.java
@@ -11,7 +11,7 @@ public class ArrayStack {
 
     public void push(int item) {
         if (top == size - 1) {
-            System.out.println("Error: Stack is full.");
+            System.out.println("Stack is full.");
             return;
         }
         int ele=array[++top];
@@ -42,18 +42,49 @@ public class ArrayStack {
     public int size() {
         return (top + 1);
     }
+    public void display() {
+        if (top == -1) {
+            System.out.println("Error: Stack is empty.");
+            return;
+        }
+        System.out.print("Current Stack: ");
+        for (int i = top; i >= 0; i--) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
 
     public static void main(String[] args) {
-        ArrayStack stack = new ArrayStack(10);
-        stack.push(1);
-        stack.push(2);
-        stack.push(3);
-        stack.push(4);
-        stack.push(5);
-        System.out.println("Size of stack is " + stack.size());
-        System.out.println("Top element of stack is " + stack.peek());
-        stack.pop();
-        System.out.println("Top element of stack after pop is  " + stack.peek());
-        System.out.println("Is the stack empty? : "+stack.isEmpty());
+        ArrayStack stack1 = new ArrayStack(500);
+        long start1 = System.nanoTime();
+        stack1.push(8);
+        stack1.push(10);
+        stack1.push(5);
+        stack1.push(11);
+        stack1.push(15);
+        stack1.push(23);
+        stack1.push(6);
+        stack1.push(18);
+        stack1.push(20);
+        stack1.push(17);
+        stack1.display();
+
+        System.out.println(stack1.pop());
+        System.out.println(stack1.pop());
+        System.out.println(stack1.pop());
+        System.out.println(stack1.pop());
+        System.out.println(stack1.pop());
+        stack1.display();
+
+        stack1.push(4);
+        stack1.push(30);
+        stack1.push(3);
+        stack1.push(1);
+        stack1.display();
+
+        long stop1 = System.nanoTime();
+        long duration1 = (stop1 - start1);
+        System.out.println("Running Time: " + duration1 + " nanoseconds");
     }
 }
+
